b076ccb000d48765f0d2fa958652c9cf2546346f
summer0573 <s2113@e-mirim.hs.kr>
2022-09-15 19:49:15 +0900

    인트로 액티비티 및 레이아웃

--- app/src/main/java/com/example/growdiary/IntroActivity.kt
+++ app/src/main/java/com/example/growdiary/IntroActivity.kt
@@ -1,4 +1,23 @@
 package com.example.growdiary
 
-class IntroActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import com.example.growdiary.MainActivity
+import com.example.growdiary.R
+import android.os.Bundle
+import android.os.Handler
+
+class IntroActivity: AppCompatActivity() {
+      override fun onCreate(savedInstanceState: Bundle?) {
+          super.onCreate(savedInstanceState)
+           setContentView(R.layout.intro_layout)
+           var handler = Handler()
+           handler.postDelayed({
+           var intent = Intent(this, MainActivity::class.java)
+           startActivity(intent)
+          }, 1000)  }
+       override fun onPause(){
+          super.onPause()
+          finish()
+       }
 }
