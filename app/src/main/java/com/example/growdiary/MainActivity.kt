625966186dab0b2e8b89c8a7a948816def179ea3
summer0573 <s2113@e-mirim.hs.kr>
2022-09-20 19:39:19 +0900

    plantAddActivity.kt 식물 추가 화면 액티비티 추가

--- app/src/main/java/com/example/growdiary/MainActivity.kt
+++ app/src/main/java/com/example/growdiary/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.growdiary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val plusButton = findViewById<Button>(R.id.plus_Btn)
+        plusButton.setOnClickListener {
+            val intent = Intent(this, plantAddActivity::class.java)
+            //intent.putExtra("userInput")
+            startActivity(intent)
+        }
     }
 }
