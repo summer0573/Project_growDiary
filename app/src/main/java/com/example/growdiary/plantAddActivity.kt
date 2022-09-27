815be192107c174b007ea2013ef512a1c928dd34
chojangg <s2115@e-mirim.hs.kr>
2022-09-27 19:31:05 +0900

    식물 종류 spinner 추가

--- app/src/main/java/com/example/growdiary/plantAddActivity.kt
+++ app/src/main/java/com/example/growdiary/plantAddActivity.kt
@@ -4,14 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.growdiary.databinding.ActivityMainBinding
+import com.example.growdiary.databinding.ActivityPlantAddBinding
 
 
 class plantAddActivity : AppCompatActivity() {
     private val GET_GALLERY_IMAGE = 200
     private var imageview: ImageView? = null
-    
+    lateinit var binding: ActivityPlantAddBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plant_add)
@@ -23,6 +29,32 @@ class plantAddActivity : AppCompatActivity() {
                 "image/*"
             )
             startActivityForResult(intent, GET_GALLERY_IMAGE)
+        }
+        binding = ActivityPlantAddBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val itemList = listOf("다육이", "채소", "과일", "허브", "반려식물")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, itemList)
+
+        binding.spinner.adapter = adapter
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position != 0) Toast.makeText(
+                    this@plantAddActivity,
+                    itemList[position],
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
         }
     }
 
