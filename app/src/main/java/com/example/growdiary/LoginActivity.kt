2841893b6ac568e949ebcac39cc03753b15eee3f
chojangg <s2115@e-mirim.hs.kr>
2022-10-11 19:59:44 +0900

    로그인 구상중

---
+++ app/src/main/java/com/example/growdiary/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.example.growdiary
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val plusButton = findViewById<Button>(R.id.plus_Btn)
+        plusButton.setOnClickListener {
+            Log.d("mytag", "plus button")
+            val intent = Intent(this, plantAddActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
