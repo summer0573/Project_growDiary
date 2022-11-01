55165777ab9656a528de74af5f5e772b53f4859c
summer0573 <s2113@e-mirim.hs.kr>
2022-11-01 18:47:09 +0900

    dateBtn에 있는 text 파이어베이스에 넘기기

--- app/src/main/java/com/example/growdiary/plantAddFirebase.kt
+++ app/src/main/java/com/example/growdiary/plantAddFirebase.kt
@@ -22,18 +22,22 @@ class plantAddFirebase : AppCompatActivity() {
         val db = Firebase.firestore
 
         var growBtn = findViewById<Button>(R.id.grow_btn)
+        var dateBtn = findViewById<Button>(R.id.date_btn)
+
         growBtn.setOnClickListener {
             val plantImage = findViewById<ImageView>(R.id.imageView)
             val plantName = findViewById<EditText>(R.id.name_EditText).text.toString()
             val plantSpinner = findViewById<Spinner>(R.id.spinner)
+            val plantDate = dateBtn.text.toString()
             //날짜
             val plants = hashMapOf(
                 "image" to plantImage,
                 "name" to plantName,
-                "Spinner" to plantSpinner
+                "Spinner" to plantSpinner,
+                "date" to plantDate
                 //날짜
             )
-            db.collection("plants").document(plantName)
+            db.collection("plants").document("trees")
                 .set(plants)
                 .addOnSuccessListener {
                     Log.d("mytag", "DocumentSnapshot successfully written!")
